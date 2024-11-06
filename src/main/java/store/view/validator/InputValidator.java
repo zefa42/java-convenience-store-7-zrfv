a5853e2fc86e2a3bb70aa2b6357554dc9330570d
Zrfv <youngoh07015@gmail.com>
2024-11-06 14:14:49 +0900

    feat: 구매 형식이 대괄호로 시작하고 끝나는지 예외 검증 기능 구현

--- src/main/java/store/view/validator/InputValidator.java
+++ src/main/java/store/view/validator/InputValidator.java
@@ -2,10 +2,17 @@ package store.view.validator;
 
 public class InputValidator {
     public static final String EMPTY_INPUT_ERROR_MESSAGE = "[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.";
+    public static final String PRODUCT_FORMAT_ERROR_MESSAGE = "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.";
 
     public static void validateEmptyInput(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(EMPTY_INPUT_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateProductFormat(String input) {
+        if (!(input.startsWith("[") && input.endsWith("]"))) {
+            throw new IllegalArgumentException(PRODUCT_FORMAT_ERROR_MESSAGE);
         }
     }
 }
--- src/test/java/store/view/validator/InputValidatorTest.java
+++ src/test/java/store/view/validator/InputValidatorTest.java
@@ -14,4 +14,12 @@ class InputValidatorTest {
                 () -> new InputValidator().validateEmptyInput(input));
         assertEquals("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.", exception.getMessage());
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"[콜라-3[", "   [콜라-1]"})
+    void 구매_상품_입력이_대괄호로_시작하고_끝나지_않으면_예외를_발생시킨다(String input) {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new InputValidator().validateProductFormat(input));
+        assertEquals("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.", exception.getMessage());
+    }
 }
